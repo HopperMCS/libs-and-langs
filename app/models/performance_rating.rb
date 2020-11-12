10936ce5b0d8071da7bfe435614d6e4939c405d1
M. Gage Morgan <gage@gages.blog>
2020-11-12 08:45:20 -0500

    Add validates that check for presence and that rating integer can only include numbers 1-5

--- app/models/performance_rating.rb
+++ app/models/performance_rating.rb
@@ -1,4 +1,7 @@
 class PerformanceRating < ApplicationRecord
   belongs_to :language
   belongs_to :library
+
+  validates :rating, :commentary, presence: true
+  validates :rating, inclusion: { in: [ 1..5 ] }
 end
