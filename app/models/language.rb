428aff362239df0bc6bbff6bde59f04f9dd49736
M. Gage Morgan <gage@gages.blog>
2020-11-12 08:44:12 -0500

    Add validates that check for presence of columns

--- app/models/language.rb
+++ app/models/language.rb
@@ -2,4 +2,6 @@ class Language < ApplicationRecord
   belongs_to :user
   has_many :performance_ratings
   has_many :libraries, through: :performance_ratings
+
+  validates :name, :source_link, presence: true
 end
