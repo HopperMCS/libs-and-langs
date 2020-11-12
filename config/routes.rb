3c486877d7ecfe5bca459a6d0bdf73c0aceadcd7
M. Gage Morgan <gage@gages.blog>
2020-11-12 14:14:59 -0500

    Start work on nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :languages
+  resources :languages do
+    resources :libraries, only: [:show, :new]
+  end
   resources :libraries
   resources :performance_ratings
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
