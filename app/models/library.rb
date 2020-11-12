b04fd3766acb950be2bdd7f33b3a2bba5da99555
M. Gage Morgan <gage@gages.blog>
2020-11-12 08:44:45 -0500

    Add validates that check for presence of data in columns and for boolean values

--- app/models/library.rb
+++ app/models/library.rb
@@ -3,4 +3,6 @@ class Library < ApplicationRecord
   has_many :performance_ratings
   has_many :languages, through: :performance_ratings
 
+  validates :name, :purpose, :compiled, :turing_complete, presence: true
+  validates :compiled, :turing_complete, inclusion: { in: [ true, false ] }
 end
