ac833790319cbfd4943c062e0877502f709ecd0a
M. Gage Morgan <gage@gages.blog>
2020-11-12 14:10:36 -0500

    Add test scope methods to PerformanceRating

--- app/models/performance_rating.rb
+++ app/models/performance_rating.rb
@@ -5,4 +5,9 @@ class PerformanceRating < ApplicationRecord
 
   validates :rating, :commentary, presence: true
   validates :rating, inclusion: { in: [ 1..5 ] }
+
+  scope :order_by_positive -> { select(rating).from(performance_ratings).order_by(rating).descending }
+  scope :order_by_negative -> { select(rating).from(performance_ratings).order_by(rating).ascending }
+
+  scope order_by_creation -> { select(created_at).from(performance_ratings).order_by(created_at).descending }
 end
