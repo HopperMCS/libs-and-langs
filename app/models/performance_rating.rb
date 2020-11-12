4b729b7503fd908cffa4211c40fb14fe3b8267cf
M. Gage Morgan <gage@gages.blog>
2020-11-12 13:46:10 -0500

    Remove belongs_to association

--- app/models/performance_rating.rb
+++ app/models/performance_rating.rb
@@ -1,6 +1,7 @@
 class PerformanceRating < ApplicationRecord
   belongs_to :language
   belongs_to :library
+  belongs_to :user
 
   validates :rating, :commentary, presence: true
   validates :rating, inclusion: { in: [ 1..5 ] }
